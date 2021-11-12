9cc740cff35ed8d919d2b0a034f3bfbb5a3fed1f
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-11-12 17:31:20 +0700

    sadasd

--- django_hospital/news/views.py
+++ django_hospital/news/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {
+
+    }
+     
+    if request.method == "POST":
+        print(request.POST)
+
+    return render(request, 'home/index.html', context)
