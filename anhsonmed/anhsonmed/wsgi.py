df6f34851e20d139fca6eb699ceb4d353f3d87fb
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-12-06 09:46:48 +0700

    rtt

--- anhsonmed/anhsonmed/wsgi.py
+++ anhsonmed/anhsonmed/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_hospital project.
+WSGI config for anhsonmed project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_hospital.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anhsonmed.settings')
 
 application = get_wsgi_application()
--- django_hospital/news/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from tinymce.models import HTMLField
-
-# Create your models here.
-
-class New(models.Model):
-    content = HTMLField()
